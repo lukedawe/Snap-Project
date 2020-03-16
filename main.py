fc1f537603ee2410bb45a7d23b5edaea795e374e
lukedawe <43144118+lukedawe@users.noreply.github.com>
2020-03-16 12:30:39 +0000

    Trying to add node lables
    Does not work, nodes turn black.

--- main.py
+++ main.py
@@ -3,23 +3,27 @@ import csv
 import pandas as pd
 import os
 import numpy as np
+import time
 
 os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 
 
 # Opens the file
 def open_file():
-    data = pd.read_csv(r'data.csv')
+    data = pd.read_csv(r'Scotland_2018.csv', sep=',', error_bad_lines=False, index_col=False, dtype='unicode',
+                       low_memory=False)
     data = data.round(3)
     return data
 
 
 def main():
+    start_time = time.time()
     data = open_file()
     df = process_data(data)
     print(df)
     generate_graph(df)
     print("Complete")
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 
 def process_data(data):
@@ -41,12 +45,17 @@ def generate_graph(df):
     roads_and_regions.setdefault("", [])
     region_array = []
     region_to_index = {}
+    index_to_region = {}
     G1 = snap.TUNGraph.New()
+    labels = snap.TIntStrH()
+    # NIdColourH = snap.TIntStrH()
     for index, row in df.iterrows():
         if not search_array(row['local_authority_name'], region_array):
             region_array.append(row['local_authority_name'])
             region_to_index[row['local_authority_name']] = index
             G1.AddNode(index)
+            # NIdColourH[index] = "red"
+            labels[index] = row['local_authority_name']
         if not row['road_name'] in roads_and_regions:
             temp = [row['local_authority_name']]
             # print("temp: ")
@@ -66,14 +75,12 @@ def generate_graph(df):
         n = 0
         for i in roads_and_regions[road]:
             if n > 0:
-                print("...........add edge.............")
                 temp = []
                 temp = roads_and_regions[road]
                 G1.AddEdge(region_to_index[i], region_to_index[temp[n - 1]])
             n += 1
 
-    print(region_array)
-    snap.DrawGViz(G1, snap.gvlCirco, "graph.png", "Scotland's Roads", True)
+    snap.DrawGViz(G1, snap.gvlCirco, "graph.png", "Scotland's Roads", True, labels)
 
 
 main()
